5f64b8d6b20fe44e30f057bdf8f6de3eb26d962c
prakhar <67971338+prakharprobably@users.noreply.github.com>
2025-06-03 16:19:10 +0530

    Added resultsAPI endpoint

--- server/certsdbwrapper.py
+++ server/certsdbwrapper.py
@@ -58,7 +58,7 @@ def genParts():
             SELECT p.pid, p.name, p.class, p.event, p.sName, s.sAddress
             FROM participants p
             JOIN schools s ON p.sid = s.sid
-            WHERE p.event = %s AND AND p.attendance = TRUE p.sid NOT IN (
+            WHERE p.event = %s AND p.attendance = TRUE AND p.sid NOT IN (
             SELECT sid FROM {win} ORDER BY rank LIMIT 3)""").format(win=win), (event,))
     conn.commit()
     close((cur,conn))
