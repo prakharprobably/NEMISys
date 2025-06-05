1c76b3d6683de367b6f7dee4bfc439078ccf7d40
prakhar <67971338+prakharprobably@users.noreply.github.com>
2025-06-06 01:53:20 +0530

    Fixed /home/ to be functional, and perms issues

--- server/router.py
+++ server/router.py
@@ -1,28 +1,40 @@
 from flask import Blueprint, render_template,request,redirect,url_for
 from . import staffdbwrapper as staffdb
 from .auth import protect, withName
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CREDS_PATH = os.path.join(BASE_DIR, 'creds.json')
+
+with open(CREDS_PATH, 'r') as f:
+    creds = json.load(f)
 
 routes= Blueprint('routes', __name__)
 
 @routes.route('/')
-@protect(["EI"])
+@protect(["TC", "EI", "SI", "CW", "RC", "RG"])
 @withName
 def home(UUID, NAME):
-    return render_template("base.html",uuid=UUID,name=NAME,body="You are allowed to access this page")
-
-    #token = request.cookies.get("token")
-    #print(f"ROUTER: attempted access with {token}")
-    #try:
-    #    PERMS = tokens[token][1]
-    #    UUID = tokens.get(token)[0]
-    #    if "EI" not in PERMS:
-    #        print(f"ROUTER@{UUID}: Access denied")
-    #        return f"{UUID} is not allowed to access this page"
-    #    else:
-    #        print(f"ROUTER@{UUID}:access granted")
-    #        return f"{UUID} is allowed to access this page"
-    #except:
-    #    print("ROUTER: redirected to login")
-    #    return redirect(url_for("auth.login"))
+    cur,conn = staffdb.open()
+    perms = staffdb.getPerms(cur, UUID)
+    staffdb.close((cur,conn))
+    if "TC" in perms or "EI" in perms:
+        events = creds["Events"]
+        event = request.form.get('event')
+        if event:
+            return redirect('/events/{event}')
+        else:
+            return render_template("home.html", uuid=UUID, name=NAME, events=events)
+    elif "SI" in perms:
+        return redirect(url_for('events.redir'))
+    elif "RC" in perms:
+        return redirect(url_for('results.home'))
+    elif "CW" in perms:
+        return redirect(url_for('certs.home'))
+    elif "RG" in perms:
+        return redirect(url_for('regatt.home'))
+    else:
+        return render_template("base.html",uuid=UUID,name=NAME,body="You are allowed to access this page")
     
 
